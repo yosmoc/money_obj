0d5d3ea60e0151510e559664c73d889f182c6118
samurai20000 <samurai20000___@___gmail.com>
2011-05-01 19:15:30 +0900

    currencyのリファクタリング

--- money.rb
+++ money.rb
@@ -1,31 +1,43 @@
 class Money
-  def initialize(amount, currency)
+  def initialize(amount)
     @amount = amount
-    @currency = currency
   end
 
-  attr_reader :amount, :currency
+  attr_reader :amount
 
   def equal(dollar)
     return @amount == dollar.amount && self.class == dollar.class
   end
 
+
   def self.dollar(dollar)
-    return Dollar.new(dollar, 'USD')
+    return Dollar.new(dollar)
   end
 
   def self.franc(franc)
-    return Franc.new(franc, 'CHF'))
+    return Franc.new(franc)
   end
 end
 
 class Dollar < Money
+  @@currency = 'USD'
+
+  def currency
+    return @@currency
+  end
+
   def times(multiplier)
     return Dollar.new(@amount * multiplier)
   end
 end
 
 class Franc < Money
+  @@currency = 'CHF'
+
+  def currency
+    return @@currency
+  end
+
   def times(multiplier)
     return Franc.new(@amount * multiplier)
   end
