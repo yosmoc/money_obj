e781ae8e9bcb28e95b09e3b1bc02c37aaa608713
samurai20000 <samurai20000___@___gmail.com>
2011-05-01 17:42:06 +0900

    todo追加

--- money.rb
+++ money.rb
@@ -1,38 +1,31 @@
 class Money
-  def initialize(amount)
+  def initialize(amount, currency)
     @amount = amount
+    @currency = currency
   end
 
-  attr_reader :amount
+  attr_reader :amount, :currency
 
   def equal(dollar)
     return @amount == dollar.amount && self.class == dollar.class
   end
 
   def self.dollar(dollar)
-    return Dollar.new(dollar)
+    return Dollar.new(dollar, 'USD')
   end
 
   def self.franc(franc)
-    return Franc.new(franc)
+    return Franc.new(franc, 'CHF'))
   end
 end
 
 class Dollar < Money
-  def currency
-    return 'USD'
-  end
-
   def times(multiplier)
     return Dollar.new(@amount * multiplier)
   end
 end
 
 class Franc < Money
-  def currency
-    return 'CHF'
-  end
-
   def times(multiplier)
     return Franc.new(@amount * multiplier)
   end
