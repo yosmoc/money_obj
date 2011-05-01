b9bf0ba120b93136bf8c35d3b5fa4a74ae75f00b
samurai20000 <samurai20000___@___gmail.com>
2011-05-01 16:58:07 +0900

    dollarのテスト、francのテストをmoneyのテストにネスト化する

--- money_spec.rb
+++ money_spec.rb
@@ -7,51 +7,52 @@ describe Money do
   it '5ドルと5CHFは同じではない' do
     Money.dollar(5).equal(Money.franc(5)).should be_false
   end
+  describe Dollar do
+    before do
+      @five = Money.dollar(5);
+    end
+ 
+    it '@fiveを2倍にしたら10になる' do
+      @five.times(2).equal(Dollar.new(10))
+    end
+
+    it '@fiveを3倍にしたら15になる' do
+      @five.times(2)
+      @five.times(3).equal(Dollar.new(15))
+    end
+
+    it '$5ドルと$5ドルは同じとなる' do
+      @five.equal(Dollar.new(5)).should be_true
+    end
+    
+    it '$5ドルと$6ドルは同じとならない' do
+      @five.equal(Dollar.new(6)).should be_false
+    end
+  end
+
+  describe Franc do
+    before do
+      @five = Money.franc(5)
+    end
+    
+    it '@fiveを2倍にしたら10になる' do
+      @five.times(2).equal(Franc.new(10))
+    end
+
+    it '@fiveを3倍にしたら15になる' do
+      @five.times(2)
+      @five.times(3).equal(Franc.new(15))
+    end
+
+    it '$5francと$5francは同じとなる' do
+      @five.equal(Franc.new(5)).should be_true
+    end
+
+    it '$5francと$6francは同じとならない' do
+      @five.equal(Franc.new(6)).should be_false
+    end
+  end
 end
 
-describe Dollar do
-  before do
-    @five = Money.dollar(5);
-  end
 
-  it '@fiveを2倍にしたら10になる' do
-    @five.times(2).equal(Dollar.new(10))
-  end
-
-  it '@fiveを3倍にしたら15になる' do
-    @five.times(2)
-    @five.times(3).equal(Dollar.new(15))
-  end
-
-  it '$5ドルと$5ドルは同じとなる' do
-    @five.equal(Dollar.new(5)).should be_true
-  end
-
-  it '$5ドルと$6ドルは同じとならない' do
-    @five.equal(Dollar.new(6)).should be_false
-  end
-end
-
-describe Franc do
-  before do
-    @five = Money.franc(5)
-  end
-
-  it '@fiveを2倍にしたら10になる' do
-    @five.times(2).equal(Franc.new(10))
-  end
-
-  it '@fiveを3倍にしたら15になる' do
-    @five.times(2)
-    @five.times(3).equal(Franc.new(15))
-  end
-
-  it '$5francと$5francは同じとなる' do
-    @five.equal(Franc.new(5)).should be_true
-  end
-
-  it '$5francと$6francは同じとならない' do
-    @five.equal(Franc.new(6)).should be_false
-  end
-end
 
